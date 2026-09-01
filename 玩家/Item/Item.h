d3f486c345fa34593c4a1a2fb8d9fb707da5b6d2
moy0781 <1966200288@qq.com>
2026-09-01 14:06:10 +0800

    Update Item.h

--- 玩家/Item/Item.h
+++ 玩家/Item/Item.h
@@ -1,1 +1,34 @@
 
+#ifndef ITEM_H
+#define ITEM_H
+
+#include "Stats.h"
+#include <map>
+#include <string>
+
+class Player;
+
+class Item
+{
+public:
+    Item(std::string id,
+         std::string type,
+         int price,
+         std::map<StatType, int> effects = {});
+
+    const std::string& getId() const;
+    const std::string& getType() const;
+    int getPrice() const;
+    const std::map<StatType, int>& getEffects() const;
+
+    void use(Player& player) const;
+    void show() const;
+
+private:
+    std::string id_;
+    std::string type_;
+    int price_;
+    std::map<StatType, int> effects_;
+};
+
+#endif
