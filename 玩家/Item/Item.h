98f941483fcf30d905fb4136dc6aa1331c67aa7e
moy0781 <1966200288@qq.com>
2026-09-01 14:42:17 +0800

    Update Item.h

--- 玩家/Item/Item.h
+++ 玩家/Item/Item.h
@@ -3,23 +3,22 @@
 #define ITEM_H
 
 #include "Stats.h"
-#include <map>
 #include <string>
+#include <vector>
+#include <utility>
 
 class Player;
 
 class Item
 {
 public:
-    Item(std::string id,
-         std::string type,
-         int price,
-         std::map<StatType, int> effects = {});
+    Item(std::string id, std::string type, int price,
+        std::vector<std::pair<StatType, int>> effects = {});
 
     const std::string& getId() const;
     const std::string& getType() const;
     int getPrice() const;
-    const std::map<StatType, int>& getEffects() const;
+    const std::vector<std::pair<StatType, int>>& getEffects() const;
 
     void use(Player& player) const;
     void show() const;
@@ -28,7 +27,7 @@ private:
     std::string id_;
     std::string type_;
     int price_;
-    std::map<StatType, int> effects_;
+    std::vector<std::pair<StatType, int>> effects_;
 };
 
 #endif
