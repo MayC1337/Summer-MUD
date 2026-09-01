f30c01c52ac5d6073041de48abfa23013568831c
moy0781 <1966200288@qq.com>
2026-09-01 13:55:15 +0800

    Update Player.h

--- 玩家/Player.h
+++ 玩家/Player.h
@@ -2,35 +2,37 @@
 #ifndef SUMMER_MUD_PLAYER_H
 #define SUMMER_MUD_PLAYER_H
 
-#include "Stats.h"
-
-#include <memory>
 #include <string>
+#include <memory>
+
+class Stats;
+class Inventory;
 
 class Player
 {
 public:
-    Player(
-        const std::string& name,
-        std::unique_ptr<Stats> stats,
-        int money = 0
-    );
+    Player(const std::string& name,
+           std::unique_ptr<Stats> stats,
+           int money = 0);
 
     const std::string& getName() const;
-
-    const Stats& getStats() const;
-    Stats& getStats();
-
     int getMoney() const;
     void changeMoney(int change);
 
+    Stats& getStats();
+    const Stats& getStats() const;
     void modifyStat(StatType type, int change);
+
+    Inventory& getInventory();
+    const Inventory& getInventory() const;
+
     void showStatus() const;
 
 private:
     std::string name_;
     std::unique_ptr<Stats> stats_;
     int money_;
+    std::unique_ptr<Inventory> inventory_;
 };
 
 #endif
