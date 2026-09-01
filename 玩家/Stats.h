25070d58300c1b6186603ff76f17a21a2dec0233
moy0781 <1966200288@qq.com>
2026-09-01 13:55:48 +0800

    Update Stats.h

--- 玩家/Stats.h
+++ 玩家/Stats.h
@@ -10,29 +10,22 @@ enum class StatType
     EQ,
     Stamina,
     Health,
-    Stress,
-    Chinese,
-    Math,
-    English,
-    Science
+    Stress
 };
+
+const char* to_string(StatType type);
 
 class Stats
 {
 public:
-    Stats(
-        int intelligence = 0,
-        int eq = 0,
-        int stamina = 0,
-        int health = 0,
-        int stress = 0,
-        int chinese = 0,
-        int math = 0,
-        int english = 0,
-        int science = 0
-    );
+    Stats(int intelligence = 0,
+          int eq = 0,
+          int stamina = 0,
+          int health = 0,
+          int stress = 0);
 
     void modify(StatType type, int change);
+    void set(StatType type, int value);
     int get(StatType type) const;
     void show() const;
 
@@ -42,13 +35,8 @@ private:
     int stamina_;
     int health_;
     int stress_;
-    int chinese_;
-    int math_;
-    int english_;
-    int science_;
 
     static int clamp(int value);
-
     int& valueOf(StatType type);
     const int& valueOf(StatType type) const;
 };
