c60331382bedf3876bbeb01c70ec584c08b4fa86
moy0781 <1966200288@qq.com>
2026-09-01 14:04:08 +0800

    Update Inventory.h

--- 玩家/Inventory/Inventory.h
+++ 玩家/Inventory/Inventory.h
@@ -1,1 +1,23 @@
 
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "Item.h"
+#include <vector>
+#include <memory>
+#include <string>
+
+class Inventory
+{
+public:
+    void addItem(std::unique_ptr<Item> item);
+    bool removeItem(const std::string& id);
+    bool hasItem(const std::string& id) const;
+    const std::vector<std::unique_ptr<Item>>& getItems() const;
+    void showItems() const;
+
+private:
+    std::vector<std::unique_ptr<Item>> items_;
+};
+
+#endif
